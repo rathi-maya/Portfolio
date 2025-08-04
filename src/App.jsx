e995299353080814378ceb193bf80f47f1068908
rathi-maya <rathim24it@psnacet.edu.in>
2025-08-04 21:03:02 +0530

    seting up the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Components/Home.jsx';
+import About from './Components/About.jsx';
+import Skills from './Components/Skills.jsx';
+import Projects from './Components/Projects.jsx';
+import Contact from './Components/Contact.jsx';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
+    <> 
+      <Home/>
+      {/* <About/> */}
+      <Skills/>
+      <Projects/>
+      <Contact/>
+
     </>
   )
 }
