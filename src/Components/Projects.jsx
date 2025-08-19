e14f43b87db4ad161b779ecd2b31dea4f75a6a2f
rathi-maya <rathim24it@psnacet.edu.in>
2025-08-19 22:31:17 +0530

    Box made

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 function Projects() {
-
+  return (
+    <div className="bg-black min-h-screen text-primary">
+        <div className='text-[32px] text-primary font-bold text-center' >Projects</div>
+        <div>
+            <div className='flex gap-4 m-4'>
+                <div className='border-2 h-[400px] w-[300px]'>
+                    Login and Sign Up page
+                </div>
+                 <div className='border-2 h-[400px] w-[300px]'>
+                    Login and Sign Up page
+                </div>
+                 <div className='border-2 h-[400px] w-[300px]'>
+                    Login and Sign Up page
+                </div>
+            </div>
+        </div>
+    </div>
+  )
 }
 
 export default Projects
