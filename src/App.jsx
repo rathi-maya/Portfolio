8cd82736fe496897013448e425cd88d500a05adb
rathi-maya <rathim24it@psnacet.edu.in>
2025-10-23 21:23:22 +0530

    use state removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar.jsx';
 import Home from './Components/Home.jsx';
 import About from './Components/About.jsx';
 import Skills from './Components/Skills.jsx';
-import Projects from './Components/Projects.jsx';
 import Contact from './Components/Contact.jsx';
 import ProjectList from './Components/ProjectList.jsx';
-import ProfilePic from './Components/profilePic.jsx';
-import Details from './Components/Details.jsx';
+
 
 
 function App() {
