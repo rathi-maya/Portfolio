52c0fcd117dd5e6ba1d7986276e41e16457c3e57
rathi-maya <rathim24it@psnacet.edu.in>
2026-01-01 20:23:44 +0530

    Hover effect to button

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,12 +1,10 @@
-import link from '../assets/link.png'
-
 function Projects(props) {
   return (
-      <div className="text-white h-[400px] w-[350px] border-2 border-primary m-5 rounded-[5px] p-5">
+      <div className="text-white h-[400px] w-[350px] border-2 border-primary m-5 rounded-[5px] p-5 cardHover ">
         <div>
         <h2 className="text-center p-2 text-xl font-bold" >{props.title}</h2>
         <p className="leading-6">{props.description}</p>
-        <h3 className="font-semibold text-lg mt-3">Tech Stack</h3>
+        <h3 className="font-semibold text-lg mt-3">⚙️ Tech Stack</h3>
            <div className="flex gap-3 ml-5 mt-2 text-primary font-semibold">
            <p>{props.techstack1}</p>
            <p>{props.techstack2}</p>
@@ -15,10 +13,10 @@ function Projects(props) {
               <p>{props.techstack5}</p>
               </div>
         
-        <h3 className="font-semibold text-lg mt-3  flex">Links</h3>
+        <h3 className="font-semibold text-lg mt-3  flex">🔗 Links</h3>
             <div className="mt-3">
-            <button className="btn"><a href={props.veiw}>View Demo</a></button>
-            <button className="btn"><a href={props.source}>Source Code</a></button>
+            <button className="btn cardHover"><a href={props.view}>View Demo</a></button>
+            <button className="btn cardHover"><a href={props.source}>Source Code</a></button>
             </div>
       </div>
       </div>
