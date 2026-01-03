055be722f00ccf327b624a34b2faf319b0fbbde1
rathi-maya <rathim24it@psnacet.edu.in>
2026-01-03 10:03:13 +0530

    Router Link Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,41 @@
 import './index.css'
-import App from './App.jsx'
-import React from "react";
+import Home from './Components/Home.jsx';
+import About from './Components/About.jsx';
+import Skills from './Components/Skills.jsx';
+import Projects from './Components/Projects.jsx';
+import Contact from './Components/Contact.jsx';
+import { RouterProvider } from 'react-router-dom';
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/about',
+    element: <About />
+  },
+  {
+    path : '/skills',
+    element: <Skills />
+  },
+  {
+    path : '/projects',
+    element: <Projects />
+  },
+  {
+    path : '/contact',
+    element: <Contact />
+  }
+
+])
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ <StrictMode>
+  <RouterProvider router={router}></RouterProvider>
+ </StrictMode>
 );
