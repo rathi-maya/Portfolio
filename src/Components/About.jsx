f8aa5a12de20ba77be44fa09531686424a69caaa
rathi-maya <rathim24it@psnacet.edu.in>
2026-01-05 22:25:16 +0530

    Responsivenes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import ProfilePic from "../assets/ai-generated.png";
+import ProfilePic from "../assets/profile.jpg";
 import SideBar from "../assets/education_sideBar.png";
 
 const About = () => {
   return (
-     <div className="flex flex-col md:flex-row min-h-screen bg-black text-white">
-      <div className="w-full md:w-1/3 md:p-10 flex-shrink-0 md:sticky md:top-0  ">
+     <div className="flex flex-col lg:flex-row min-h-screen bg-black text-white">
+      <div className="w-full lg:w-1/3 md:p-10 flex-shrink-0  
+                flex items-center justify-center">
+
         <div className="p-8 md:p-0 flex items-center justify-center">
           <img
             src={ProfilePic}
             alt="Rathi M"
-            className="rounded-lg shadow-lg w-full max-w-sm md:max-w-none 
+            className="rounded-lg shadow-lg w-full max-w-sm md:max-w-[300px] 
                        transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-primary/50"
           />
         </div>
       </div>
-
-      <div className="w-full md:w-2/3 p-10 ">
-        <h1 className="text-4xl font-bold mb-4 text-primary headHover">
+      <div className="w-full lg:w-2/3 p-10  md:items-center">
+       <div className="">
+        <div className=" pl-[70px] lg:pl-[250px] md:pl-[300px] sm:pl-[210px]">
+        <h1 className="headHover mb-4 ">
           About Me
         </h1>
-        <p className="mb-8 text-lg leading-relaxed">
+        </div>
+        <p className="mb-8 text-lg leading-relaxed ">
           Hi there! I'm <strong style={{ color: "#00ffab" }}>Rathi M</strong>, a
           second-year B.TECH IT student with a strong passion for{" "}
-          <strong style={{ color: "#00ffab" }}>frontend development</strong>. My
+          <strong style={{ color: "#00ffab" }}>Frontend Development</strong>. My
           curiosity for building digital interfaces started early in college,
           and I've been exploring technologies ever since. I started by building
           simple static pages with <strong className="text-white">HTML</strong>{" "}
@@ -33,10 +37,11 @@ const About = () => {
           learning the <strong className="text-white">MERN stack</strong>,
           aiming to become a full-stack developer.
         </p>
-
+            <div className=" pl-[70px] lg:pl-[250px] md:pl-[300px] sm:pl-[210px]">
         <h2 class="text-3xl font-bold mb-4 text-primary headHover">
           Education
         </h2>
+        </div>
         {/* college */}
         <div class="flex items-start space-x-6">
           <div class="flex-shrink-0">
@@ -91,9 +96,9 @@ const About = () => {
           </div>
         </div>
 
-        {/* 9841277240 */}
 
       </div>
+    </div>
     </div>
   );
 };
