a7cd5817eee171025d9975c32a0d44b008679888
rathi-maya <rathim24it@psnacet.edu.in>
2026-01-06 09:31:04 +0530

    Restore to strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,11 @@
 import './index.css'
-import App from './App.jsx';
-import Home from './Components/Home.jsx';
-import About from './Components/About.jsx';
-import Skills from './Components/Skills.jsx';
-import ProjectList from './Components/ProjectList.jsx';  
-import Contact from './Components/Contact.jsx';
-import { RouterProvider } from 'react-router-dom';
+import App from './App.jsx'
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import Layout from './Layout.jsx';
-import { createBrowserRouter } from 'react-router-dom';
-const router = createBrowserRouter([
-  {
-    element : <Layout />,
-    children : [
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/about',
-    element: <About />
-  },
-  {
-    path : '/skills',
-    element: <Skills />
-  },
-  {
-    path : '/projects',
-    element: <ProjectList />
-  },
-  {
-    path : '/contact',
-    element: <Contact />
-  },
-],
-},
-])
-
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <StrictMode>
-  <RouterProvider router={router}></RouterProvider>
- </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
